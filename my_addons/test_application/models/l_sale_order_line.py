a86a73844e149b05e55fb63e730187d0c149799e
SilverTune <3134874094@qq.com>
2026-07-16 15:06:09 +0800

    多对多的关系，以及数据库中间表的关系自定义，添加对应模型

--- my_addons/test_application/models/l_sale_order_line.py
+++ my_addons/test_application/models/l_sale_order_line.py
@@ -11,9 +11,17 @@ class LSaleOrderLine(models.Model):
     # 因此需要在manifest文件里去依赖product模块
     product_id = fields.Many2one('product.product', string="产品", help="这是我的产品")
     order_id = fields.Many2one('l.sale.order', string="订单", help="这是我的订单")
+
     # flout类型的字段，dights是小数点后几位,Product Unit是产品单价单位的意思，在odoo的设置里调整
     # price_unit = fields.Float(string="单价", help="这是我的单价", digits=2)在19.0版本中已经不支持了，必须使用digits='Product Unit'，否则会报错
     # price_unit = fields.Float(string="单价", help="这是我的单价", digits=(16,2)),digits=(最大值,小数点后的值)
     price_unit = fields.Float(string="单价", help="这是我的单价",digits='Product Unit')
     amount_total = fields.Float(string="总价", digits=(16,2))
     qty = fields.Float(string="数量", digits='Product Unit')
+
+    # 这里第二个因素是relation，第三个因素是column1，第四个因素是column2，第五个因素是string
+    # column1是当前模型的字段，column2是关联模型的字段，这里是可以自定义的
+    # 也是可以不写的，写了就会在数据库里生成一个表，表名是relation，字段是column1和column2
+    tax_ids = fields.Many2many('account.tax', 'l_sale_order_line_account_tax_rel',
+                               'l_sale_order_line','account_tax_id'
+                               ,string="税项")
